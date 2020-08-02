db4ce326af797767419a65fe22bfee06f0e73a83
xiaobinggg@gmail.com <xiaobinggg@gmail.com>
2020-08-02 23:20:33 +0800

    feat: 事件添加

--- main.js
+++ main.js
@@ -1,15 +1,51 @@
-import {ToyReact, Component} from './ToyReact'
+import { ToyReact, Component } from './ToyReact'
 
-class MyComponent extends Component {
+class Square extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            value: null
+        }
+    }
     render() {
-        return <div>
-            {this.children}
-        </div>
+        return (
+            <button className="square" onClick={()=>{this.setState({value: 'X'})}}>
+                {this.state.value ? this.state.value : ""}
+            </button>
+        );
+    }
+}
+class Board extends Component {
+    renderSquare(i) {
+        return (
+            <Square value={i} />
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
     }
 }
 
-let a = <MyComponent id="ida" name="namea">
-    <div>aaa</div>
-</MyComponent>
+
+let a = <Board />
 
 ToyReact.render(a, document.body)
